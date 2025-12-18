9fb59353710c15c82a8c514fc21a24900365c7a2
Kevin Hernandez <khernandez0810@gmail.com>
2025-12-17 21:48:15 -0700

    redesign

--- components/Features.jsx
+++ components/Features.jsx
@@ -5,30 +5,6 @@ import {useEffect, useState} from 'react'
 const Features = () => {
     const [index, setIndex] = useState(0)
 
-    // const handleArrow = (direction) => {
-    //     if (direction === "l"){
-    //         setIndex(index !==0 ? index-1 : 2)
-    //     }
-    //     if (direction === "r"){
-    //         setIndex(index !==2 ? index+1 : 0)
-    //     }
-    //     console.log(index)
-    // }
-
-    // useEffect(() => {
-    //     const switchImage = () => {
-    //         setIndex((prevIndex) => (prevIndex !== images.length -1 ? prevIndex + 1 : 0));
-    //     };
-
-    //     const timer = setInterval(switchImage, 3000);
-
-    //     return () => clearInterval(timer);
-    // }, []);
-
-
-
-
-
   return (
     <div className={styles.container}>
         <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
